cfe347f9de3f982fcf9311877435a85717ba1fa8
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 08:34:13 +0900

    delete title tag

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
 </head>
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
