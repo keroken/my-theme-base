254cbfde08165c11028db2d3f7761e80db64f151
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 08:49:00 +0900

    display title and content on page

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
 
   <nav></nav>
 
-  <main></main>
+  <main>
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+  </main>
 
   <footer></footer>
 
