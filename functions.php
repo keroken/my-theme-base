f17945ac8371037080ed7d7d29cef2ede93c529f
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 08:52:19 +0900

    add thumbnail to page

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 // Basic setup
 function mytheme_setup() {
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'mytheme_setup');
--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 
   <main>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      <?php if(has_post_thumbnail()): ?>
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" style="max-width: 100%;">
+      <?php endif; ?>
       <h1><?php the_title(); ?></h1>
       <?php the_content(); ?>
     <?php endwhile; endif; ?>
