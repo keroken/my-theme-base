d74465692e48498b894f545cfa1140fd94699953
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 08:31:51 +0900

    update setup

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Basic setup
-function mytheme_a_setup() {
+function mytheme_setup() {
   add_theme_support('title-tag');
 }
-add_action('after_setup_theme', 'mytheme_a_setup');
+add_action('after_setup_theme', 'mytheme_setup');
