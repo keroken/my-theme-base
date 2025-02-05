47fedaedca70f2d28c5365b5326d6473bef54d68
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 09:27:15 +0900

    add custom menu support to function.php

--- functions.php
+++ functions.php
@@ -2,7 +2,15 @@
 
 // Basic setup
 function mytheme_setup() {
+  // Add title tag support
   add_theme_support('title-tag');
+
+  // Add post thumbnail support
   add_theme_support('post-thumbnails');
+
+  // Add custom menu support
+  register_nav_menus(array(
+    'primary' => 'Primary Menu',
+  ));
 }
 add_action('after_setup_theme', 'mytheme_setup');
