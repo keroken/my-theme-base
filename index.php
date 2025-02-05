976e1bf46a6c1efa3ff22010f578e723b888a69a
keroken <kenichi.shinagawa@gmail.com>
2025-02-05 09:05:03 +0900

    output post class

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
 
   <main>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
-      <?php if(has_post_thumbnail()): ?>
-        <figure><?php the_post_thumbnail(); ?></figure>
-      <?php endif; ?>
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
+      <article <?php post_class(); ?>>
+        <?php if(has_post_thumbnail()): ?>
+          <figure><?php the_post_thumbnail(); ?></figure>
+        <?php endif; ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      </article>
     <?php endwhile; endif; ?>
   </main>
 
