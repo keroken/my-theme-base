b1971b8e13ccf10892826ff9fff2eaf2b00191ff
keroken <kenichi.shinagawa@gmail.com>
2025-02-10 00:50:28 +0900

    update index to style posts page

--- index.php
+++ index.php
@@ -1,43 +1,32 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-  <?php wp_body_open(); ?>
+<?php get_header(); ?>
 
-  <header>
-    <a href="<?php echo esc_url(home_url('/')); ?>">
-      <?php bloginfo('name'); ?>
-    </a>
-  </header>
-
-  <?php if (has_nav_menu('primary')): ?>
-  <nav>
-    <?php wp_nav_menu(array(
-      'theme_location' => 'primary',
-    )); ?>
-  </nav>
-  <?php endif; ?>
-
-  <main>
-    <?php if(have_posts()): while(have_posts()): the_post(); ?>
-      <article <?php post_class(); ?>>
-        <?php if(has_post_thumbnail()): ?>
-          <figure><?php the_post_thumbnail(); ?></figure>
-        <?php endif; ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-      </article>
-    <?php endwhile; endif; ?>
+<div class="front-bg">
+  <div class="page-hero-container">
+    <div class="page-hero-message">
+      <h1>Stories</h1>
+    </div>
+  </div>
+  <main class="post-container">
+    <div class="page-content">
+      <h3>UNITED IN GOD'S GREAT STORIES</h3>
+      <div class="message-text about-content">
+        <p>Sharing from students, volunteers and staff</p>
+      </div>
+    </div>
+    <div class="post-wrapper">
+      <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <article class ="post-frame" <?php post_class(); ?>>
+          <?php if(has_post_thumbnail()): ?>
+            <figure class="post-thumbnail"><?php the_post_thumbnail(); ?></figure>
+          <?php endif; ?>
+          <h3 class="post-title"><?php the_title(); ?></h3>
+          <span class="message-text">
+            <?php the_excerpt(); ?>
+          </span>
+        </article>
+      <?php endwhile; endif; ?>
+    </div>
   </main>
+</div>
 
-  <footer>
-    <?php bloginfo('name'); ?>
-  </footer>
-
-  <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
