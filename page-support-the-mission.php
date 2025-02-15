13eb195ff7148433ee0f1a2434ffa0f37b6db57e
keroken <kenichi.shinagawa@gmail.com>
2025-02-15 11:26:26 +0900

    add supporter page templates

---
+++ page-support-the-mission.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ */
+get_header('supporter'); 
+$breadcrumbs_show 			= get_post_meta(get_the_ID(), 'breadcrumbs_show', true);
+?>
+
+<div class="front-bg">
+  <div class="page-hero-container">
+    <div class="hero-overlay"></div>
+    <div class="page-hero-message">
+      <h1><?php the_title(); ?></h1>
+    </div>
+  </div>
+  <div class="content-area" id="content-area">
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+      <section <?php post_class(); ?>>
+        <div class="content-wrapper">
+          <div class="message-text page-content">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </section>
+
+    <?php endwhile; endif; ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
