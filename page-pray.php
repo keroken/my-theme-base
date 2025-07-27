da974a1f8c16ba9120b4522db744786c788ff918
keroken <kenichi.shinagawa@gmail.com>
2025-07-27 19:34:27 +0900

    update pray page - update uplift link and add doorways signup link

--- page-pray.php
+++ page-pray.php
@@ -24,10 +24,12 @@ $breadcrumbs_show = get_post_meta(get_the_ID(), 'breadcrumbs_show', true);
           <div class="message-text-supporter page-content-supporter prayer-content">
             <h3>Prayer Guide</h3>
             <p>Become a part of our ministry team by praying for God’s work among international students</p>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/prayer-guide.png" alt="Prayer Guide">
             <div class="prayer-guide-frame">
               <h4>Join Us In Prayer</h4>
               <p>Develop the habit of praying for international students.</p>
+              <a href="https://app.textinchurch.com/connect-cards/cKY7PqTt0w3eVoW9dSWu" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/prayer-guide.png" alt="Prayer Guide">
+              </a>
               <em><strong><a href="https://app.textinchurch.com/connect-cards/cKY7PqTt0w3eVoW9dSWu" target="_blank" rel="noopener noreferrer">Sign up for Uplift: texted requests to pray for international students</a>.</strong></em>
               <p>You’ll receive:</p>
               <ol class="prayer-guide-list">
@@ -38,6 +40,7 @@ $breadcrumbs_show = get_post_meta(get_the_ID(), 'breadcrumbs_show', true);
             </div>
             <h3>Subscribe to DoorWays</h3>
             <p>Our quarterly Doorways newsletter (online or hard copy) is loaded with stories about God's work among international students.</p>
+            <em><strong><a href="https://ismc.ca/doorways/" target="_blank" rel="noopener noreferrer">Subscribe to DoorWays</a></strong></em>
             <h3>33 Days of Prayer</h3>
             <p>Prayer is the foundation of our ministry. We believe that prayer is the key to unlocking the doors of international students' hearts. We invite you to join us in praying for the international students in your city.</p>
           </div>
