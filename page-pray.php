efc2fb117095ac48532611addf60eeb75f72b3e1
keroken <kenichi.shinagawa@gmail.com>
2025-07-29 00:55:08 +0900

    update pray page layout

--- page-pray.php
+++ page-pray.php
@@ -28,7 +28,7 @@ $breadcrumbs_show = get_post_meta(get_the_ID(), 'breadcrumbs_show', true);
               <h4>Join Us In Prayer</h4>
               <p>Develop the habit of praying for international students.</p>
               <a href="https://app.textinchurch.com/connect-cards/cKY7PqTt0w3eVoW9dSWu" target="_blank" rel="noopener noreferrer">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/prayer-guide.png" alt="Prayer Guide">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/prayer-guide.jpg" alt="Prayer Guide">
               </a>
               <p><em><strong><a href="https://app.textinchurch.com/connect-cards/cKY7PqTt0w3eVoW9dSWu" target="_blank" rel="noopener noreferrer">Sign up for Uplift: texted requests to pray for international students</a>.</strong></em></p>
               <p>You’ll receive:</p>
@@ -41,8 +41,10 @@ $breadcrumbs_show = get_post_meta(get_the_ID(), 'breadcrumbs_show', true);
             <h3>Subscribe to DoorWays</h3>
             <p>Our quarterly Doorways newsletter (online or hard copy) is loaded with stories about God's work among international students.</p>
             <p><em><strong><a href="https://ismc.ca/doorways/" target="_blank" rel="noopener noreferrer">Subscribe to DoorWays</a></strong></em></p>
-            <h3>33 Days of Prayer</h3>
-            <p>Prayer is the foundation of our ministry. We believe that prayer is the key to unlocking the doors of international students' hearts. We invite you to join us in praying for the international students in your city.</p>
+            <div class="prayer-guide-frame">
+              <h3>33 Days of Prayer</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/33days_of_prayer.jpg" alt="33 Days of Prayer">
+            </div>
           </div>
         </div>
       </section>
