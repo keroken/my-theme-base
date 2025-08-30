92e0ef1ce74f824462ae28f22cae04b085027bf9
keroken <kenichi.shinagawa@gmail.com>
2025-08-31 01:22:32 +0900

    add hero section to find your city page

--- page-find-your-city.php
+++ page-find-your-city.php
@@ -6,16 +6,34 @@
 get_header(); ?>
 
 <div class="front-bg">
-  <div class="content-area">
-    <div class="find-your-city-container">
-      <div class="map-container">
-        <div id="canada-map" style="height: 600px; width: 100%;"></div>
-      </div>
-      <div class="city-info" id="city-info">
-        <h1>Select a City</h1>
-        <p>Click on a city marker to view more information.</p>
+<div class="page-hero-area">
+    <div class="page-hero-inner">
+      <div class="page-hero-container">
+        <div class="hero-overlay"></div>
+        <div class="page-hero-message">
+          <h1><?php the_title(); ?></h1>
+        </div>
       </div>
     </div>
+  </div>
+  <div class="content-area">
+
+    <div class="content-wrapper">
+      <div class="message-text page-content">
+        <h2>Connect with our Staff in your city</h2>
+        <p>Click on a city marker to view more information.</p>
+        <div class="find-your-city-container">
+          <div class="map-container">
+            <div id="canada-map" style="height: 600px; width: 100%;"></div>
+          </div>
+          <div class="city-info" id="city-info">
+            <h3>Select a City</h3>
+            <p>Click on a city marker to view more information.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <span class="go-back-link"><a href="javascript:history.back()">Go Back</a></span>
   </div>
 </div>
