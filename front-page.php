24a3bab0e3253a0115fbb14503b97ebe8fd0bf7e
keroken <kenichi.shinagawa@gmail.com>
2025-10-12 00:25:37 +0900

    update front-page with quotes and archive previous page

--- front-page.php
+++ front-page.php
@@ -64,64 +64,91 @@ about Jesus in Jerusalem and took the gospel home with them.</p>
 forth has been part of God's strategy since the early church and continues today with 
 diaspora communities like international students.</p>
       </div>
-      <div class="story-container">
-        <div class="story-canvas">
-          <div class="circle circle-01 fingerprint fade"></div>
-          <div class="circle circle-03 fingerprint fade"></div>
-          <div class="circle circle-04 fingerprint fade"></div>
-          <div class="circle circle-09 fingerprint fade"></div>
-          <div class="circle circle-11 fingerprint fade"></div>
-          <div class="circle circle-10 logo-circle fade"></div>
-          <div class="story-circle circle-02">
-            <button class="story-circle-button" id="story-button-01" aria-label="View Hatsumi's story">
-              <div class="story-overlay"></div>
-              <div class="story-label">Hatsumi</div>
-            </button>
+      <div class="quote-container">
+        <div class="quote-item-container">
+          <div class="quote-item-left">
+            <div class="quote-line">
+              <span class="quote-marker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 10c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.318.142-.686.238-1.028.466c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.945c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 6.5 10zm11 0c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.317.143-.686.238-1.028.467c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.944c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 17.5 10z"/></svg>
+              </span>
+              <div class="quote-text-container">
+                <p class="quote-text">
+                  When I first came to Canada, I found it difficult to adjust. Lifestyle changes, though drastic, turned out to be manageable. What was harder was dealing with the loneliness, the low self-esteem, and the feeling of not belonging. At ISMC, I felt welcomed and recognized."
+                </p>
+                <p class="quote-author">Charlie, China</p>
+              </div>
+            </div>
           </div>
-          <div class="story-circle circle-05">
-            <button class="story-circle-button" id="story-button-02" aria-label="View Vinu's story">
-              <div class="story-overlay"></div>
-              <div class="story-label">Vinu</div>
-            </button>
+          <div class="circle-image image-01"></div>
+        </div>
+        <div class="quote-item-container">
+          <div class="circle-image image-02"></div>
+          <div class="quote-item-right">
+            <div class="quote-line">
+              <span class="quote-marker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 10c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.318.142-.686.238-1.028.466c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.945c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 6.5 10zm11 0c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.317.143-.686.238-1.028.467c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.944c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 17.5 10z"/></svg>
+              </span>
+              <div class="quote-text-container">
+                <p class="quote-text">
+                  The volunteers at ISMC were very welcoming. They treated us like part of their own family, and they stood with us in the best possible way, whenever we needed them."
+                </p>
+                <p class="quote-author">Rasesh, India</p>
+              </div>
+            </div>
           </div>
-          <div class="story-circle circle-06">
-            <button class="story-circle-button" id="story-button-03" aria-label="View Yan's story">
-              <div class="story-overlay"></div>
-              <div class="story-label">Yan</div>
-            </button>
+        </div>
+        <div class="quote-item-container">
+          <div class="quote-item-left">
+            <div class="quote-line">
+              <span class="quote-marker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 10c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.318.142-.686.238-1.028.466c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.945c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 6.5 10zm11 0c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.317.143-.686.238-1.028.467c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.944c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 17.5 10z"/></svg>
+              </span>
+              <div class="quote-text-container">
+                <p class="quote-text">
+                  The ISMC volunteers and organizers made an effort to create a space that felt safe, encouraging, and open—whether we were enjoying a meal together, playing board games, going on outdoor trips, or engaging in deeper conversations about life and spirituality."
+                </p>
+                <p class="quote-author">Naya, Mauritius</p>
+              </div>
+            </div>
           </div>
-          <div class="story-circle circle-07">
-            <button class="story-circle-button" id="story-button-04" aria-label="View Mindy's story">
-              <div class="story-overlay"></div>
-              <div class="story-label">Mindy</div>
-            </button>
+          <div class="circle-image image-03"></div>
+        </div>
+        <div class="quote-item-container">
+          <div class="circle-image image-04"></div>
+          <div class="quote-item-right">
+            <div class="quote-line">
+              <span class="quote-marker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 10c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.318.142-.686.238-1.028.466c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.945c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 6.5 10zm11 0c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.317.143-.686.238-1.028.467c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.944c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 17.5 10z"/></svg>
+              </span>
+              <div class="quote-text-container">
+                <p class="quote-text">
+                  The ISMC team welcomed us into many events to immerse us in Canadian culture. All of my strongest friends were born through some connection that occurred during an ISMC activity. I am so grateful for all the support we have received as international students from ISMC."
+                </p>
+                <p class="quote-author">Karen, Ecuador</p>
+              </div>
+            </div>
+            </div>
           </div>
-          <div class="story-circle circle-08">
-            <button class="story-circle-button" id="story-button-05" aria-label="View Kenji's story">
-              <div class="story-overlay"></div>
-              <div class="story-label">Kenji</div>
-            </button>
+          <div class="quote-item-container">
+          <div class="quote-item-left">
+            <div class="quote-line">
+              <span class="quote-marker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 10c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.318.142-.686.238-1.028.466c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.945c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 6.5 10zm11 0c-.223 0-.437.034-.65.065c.069-.232.14-.468.254-.68c.114-.308.292-.575.469-.844c.148-.291.409-.488.601-.737c.201-.242.475-.403.692-.604c.213-.21.492-.315.714-.463c.232-.133.434-.28.65-.35l.539-.222l.474-.197l-.485-1.938l-.597.144c-.191.048-.424.104-.689.171c-.271.05-.56.187-.882.312c-.317.143-.686.238-1.028.467c-.344.218-.741.4-1.091.692c-.339.301-.748.562-1.05.944c-.33.358-.656.734-.909 1.162c-.293.408-.492.856-.702 1.299c-.19.443-.343.896-.468 1.336c-.237.882-.343 1.72-.384 2.437c-.034.718-.014 1.315.028 1.747c.015.204.043.402.063.539l.025.168l.026-.006A4.5 4.5 0 1 0 17.5 10z"/></svg>
+              </span>
+              <div class="quote-text-container">
+                <p class="quote-text">
+                  I used to pray for friendships and connections every day and I have those connections and community now and I’m so super grateful for that."
+                </p>
+                <p class="quote-author">Thelma, Zimbabwe</p>
+              </div>
+            </div>
           </div>
+          <div class="circle-image image-05"></div>
+        </div>
         </div>
       </div>
     </section>
   </div>
 </div>
-
-<dialog class="story-modal">
-  <div id="dialogInputArea">
-    <div class="dialog-header">
-        <p id="story-title">Story</p>
-        <button class="close-button">Close Story</button>
-    </div>
-    <div class="story-body">
-      <div class="story-image-container">
-        <img id="story-image" src="" alt="Story image" />
-      </div>
-      <div id="story-content"></div>
-    </div>
-    <button class="close-button-bottom">Close Story</button>
-  </div>
-</dialog>
 
 <?php get_footer(); ?>
