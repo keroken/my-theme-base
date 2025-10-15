4dfe9b325bdbb8ded07cd58aa9e3c1eaf14320bb
keroken <kenichi.shinagawa@gmail.com>
2025-10-16 00:07:30 +0900

    add instagram and facebook links on the footer

--- footer.php
+++ footer.php
@@ -34,8 +34,43 @@
               </div>
               <div class="contact-line">
                 <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-                <span>info@ismc.ca</span>
+                <span><a href="mailto:info@ismc.ca">info@ismc.ca</a></span>
               </div>
+              <div class="contact-line">
+                <svg id="Layer_1" data-name="Layer 1" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"><defs><style>.cls-1{fill:currentColor;}</style></defs><path class="cls-1" d="M295.42,6c-53.2,2.51-89.53,11-121.29,23.48-32.87,12.81-60.73,30-88.45,57.82S40.89,143,28.17,175.92c-12.31,31.83-20.65,68.19-23,121.42S2.3,367.68,2.56,503.46,3.42,656.26,6,709.6c2.54,53.19,11,89.51,23.48,121.28,12.83,32.87,30,60.72,57.83,88.45S143,964.09,176,976.83c31.8,12.29,68.17,20.67,121.39,23s70.35,2.87,206.09,2.61,152.83-.86,206.16-3.39S799.1,988,830.88,975.58c32.87-12.86,60.74-30,88.45-57.84S964.1,862,976.81,829.06c12.32-31.8,20.69-68.17,23-121.35,2.33-53.37,2.88-70.41,2.62-206.17s-.87-152.78-3.4-206.1-11-89.53-23.47-121.32c-12.85-32.87-30-60.7-57.82-88.45S862,40.87,829.07,28.19c-31.82-12.31-68.17-20.7-121.39-23S637.33,2.3,501.54,2.56,348.75,3.4,295.42,6m5.84,903.88c-48.75-2.12-75.22-10.22-92.86-17-23.36-9-40-19.88-57.58-37.29s-28.38-34.11-37.5-57.42c-6.85-17.64-15.1-44.08-17.38-92.83-2.48-52.69-3-68.51-3.29-202s.22-149.29,2.53-202c2.08-48.71,10.23-75.21,17-92.84,9-23.39,19.84-40,37.29-57.57s34.1-28.39,57.43-37.51c17.62-6.88,44.06-15.06,92.79-17.38,52.73-2.5,68.53-3,202-3.29s149.31.21,202.06,2.53c48.71,2.12,75.22,10.19,92.83,17,23.37,9,40,19.81,57.57,37.29s28.4,34.07,37.52,57.45c6.89,17.57,15.07,44,17.37,92.76,2.51,52.73,3.08,68.54,3.32,202s-.23,149.31-2.54,202c-2.13,48.75-10.21,75.23-17,92.89-9,23.35-19.85,40-37.31,57.56s-34.09,28.38-57.43,37.5c-17.6,6.87-44.07,15.07-92.76,17.39-52.73,2.48-68.53,3-202.05,3.29s-149.27-.25-202-2.53m407.6-674.61a60,60,0,1,0,59.88-60.1,60,60,0,0,0-59.88,60.1M245.77,503c.28,141.8,115.44,256.49,257.21,256.22S759.52,643.8,759.25,502,643.79,245.48,502,245.76,245.5,361.22,245.77,503m90.06-.18a166.67,166.67,0,1,1,167,166.34,166.65,166.65,0,0,1-167-166.34" transform="translate(-2.5 -2.5)"/></svg>
+                <span><a href="https://www.instagram.com/ismc.canada/" target="_blank" rel="noopener">Instagram</a></span>
+              </div>
+              <?php
+              // Check if we're on a supporter page by looking for supporter-bg class in the body
+              $body_classes = get_body_class();
+              $is_supporter_page = false;
+
+              // Alternative method: check if current template uses supporter header
+              global $template;
+              if (
+                strpos($template, 'page-') !== false ||
+                strpos($template, 'single-') !== false ||
+                strpos($template, 'index.php') !== false
+              ) {
+                // Check if this page template calls get_header('supporter')
+                $template_content = file_get_contents($template);
+                if (
+                  strpos($template_content, "get_header('supporter')") !== false
+                ) {
+                  $is_supporter_page = true;
+                }
+              }
+
+              // Display Facebook link only on supporter pages
+              if ($is_supporter_page): ?>
+              <div class="contact-line">
+                <svg class="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fill="currentColor">
+                  <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.2 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/>
+                </svg>
+                <span><a href="https://www.facebook.com/ismc.ca" target="_blank" rel="noopener">Facebook</a></span>
+              </div>
+              <?php endif;
+              ?>
             </div>
 					</div>
 					<div class="footer-column">
@@ -47,14 +82,14 @@
 					<div class="footer-credits">
 						<p class="footer-copyright">&copy;
 							<?php
-                /* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-                $date_format = _x('Y', 'copyright date format');
-                if (function_exists('wp_date')) {
-                  echo wp_date($date_format);
-                } else {
-                  echo date_i18n($date_format);
-                }
-              ?>
+       /* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
+       $date_format = _x('Y', 'copyright date format');
+       if (function_exists('wp_date')) {
+         echo wp_date($date_format);
+       } else {
+         echo date_i18n($date_format);
+       }
+       ?>
 							<?php bloginfo('name'); ?>
 						</p><!-- .footer-copyright -->
 						<?php if (function_exists('the_privacy_policy_link')) {
