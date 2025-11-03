dfc82e8bd9340812f3e9c85fc18a3880eb99e7e8
keroken <kenichi.shinagawa@gmail.com>
2025-11-03 14:39:54 +0900

    make the toggle into two-part switch

--- content-page-toggle.php
+++ content-page-toggle.php
@@ -5,11 +5,13 @@ global $ismc_page_type;
 $is_supporter_page = isset($ismc_page_type) && $ismc_page_type === "supporter";
 ?>
 <div class="content-page-toggle-container">
-  <div class="header-navigation-page-toggle">
+  <div class="page-toggle-switch">
     <?php if ($is_supporter_page): ?>
-      <a href="<?php echo esc_url(home_url("/")); ?>">Student</a><span class="toggle-separator">|</span>Volunteer
+      <a href="<?php echo esc_url(home_url("/")); ?>" class="toggle-option">Student</a>
+      <span class="toggle-option toggle-option-active">Volunteer</span>
     <?php else: ?>
-      Student<span class="toggle-separator">|</span><a href="<?php echo esc_url(home_url("/find-out")); ?>">Volunteer</a>
+      <span class="toggle-option toggle-option-active">Student</span>
+      <a href="<?php echo esc_url(home_url("/find-out")); ?>" class="toggle-option">Volunteer</a>
     <?php endif; ?>
   </div>
 </div>
